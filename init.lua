13c6b5ecda96cec7c828f9959a886d94829931eb
MikeSilv4 <mike.oli.silva1@gmail.com>
2024-10-06 15:09:08 -0300

    change: change init and plugins

--- init.lua
+++ init.lua
@@ -1,3 +1,11 @@
 require("settings")
 require("maps")
-require("plugins")
+require('neo-tree').setup {
+    filesystem = {
+        filtered_items = {
+            hide_dotfiles = false,  -- Muda para false para mostrar arquivos ocultos
+            hide_gitignored = false,  -- Mantenha como true se não quiser ver arquivos gitignored
+        },
+    },
+}
+
